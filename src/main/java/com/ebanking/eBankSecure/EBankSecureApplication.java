36a9f9b15b849a0b1b6d8169ed9bc70047a88523
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-12-02 12:09:45 +0100

    first commit

---
+++ src/main/java/com/ebanking/eBankSecure/EBankSecureApplication.java
@@ -0,0 +1,13 @@
+package com.ebanking.eBankSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebanking/eBankSecure/EBankSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanking.eBankSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
