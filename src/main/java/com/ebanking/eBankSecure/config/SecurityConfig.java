603efd314baab426eb83fd9d8ef3736f4c2d173b
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-12-09 12:36:19 +0100

    test profile

--- src/main/java/com/ebanking/eBankSecure/config/SecurityConfig.java
+++ src/main/java/com/ebanking/eBankSecure/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ebanking.eBankSecure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Profile("!test") // Exclude this configuration when the 'test' profile is active
 public class SecurityConfig {
 
     @Bean
---
+++ src/main/java/com/ebanking/eBankSecure/config/TestPasswordEncoderConfig.java
@@ -0,0 +1,25 @@
+package com.ebanking.eBankSecure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Profile("test")
+public class TestPasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return true;
+            }
+        };
+    }
+}
